f00b140a6613e8e189a539835911fda3494144a6
syagawa <syagawapp@gmail.com>
2021-01-27 12:27:32 +0900

    Change led.py

--- led.py
+++ led.py
@@ -3,5 +3,21 @@ from time import sleep
 import os
 
 LED1 = LED(17)
+LED2 = LED(18)
+LED3 = LED(27)
+LED4 = LED(22)
+LED5 = LED(25)
+LED6 = LED(12)
+LED7 = LED(13)
+LED8 = LED(19)
+
 
 LED1.on()
+LED2.on()
+LED3.on()
+LED4.on()
+LED5.on()
+LED6.on()
+LED7.on()
+LED8.on()
+
