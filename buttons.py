27bfc8275aad879fc7d24a0da1f49c8e40eb08a7
syagawa <syagawapp@gmail.com>
2021-01-28 21:22:12 +0900

    Fix allLeds method

--- buttons.py
+++ buttons.py
@@ -4,6 +4,8 @@ import os
 
 leds = [17, 18, 27, 22, 25, 12, 13, 19]
 buttons = [21, 16, 20]
+OFF = 0
+ON = 1
 
 def getLed(n):
     if n == 0:
@@ -29,23 +31,23 @@ def getAllLeds():
     arr = []
     for i in leds:
         led = getLed(i)
-        arr.push(led)
+        arr.append(led)
     return arr
 
-def offAllLeds():
-    arr = getAllLeds()
-    for ld in arr:
-        ld.off()
-
-def onAllLeds():
-    arr = getAllLeds()
-    for ld in arr:
-        ld.on()
+def allLeds(mode):
+    for i in leds:
+        ld = getLed(i)
+        if mode == OFF:
+            ld.off()
+        elif mode == ON:
+            ld.on()
 
 
-onAllLeds()
+
+
+allLeds(ON)
 sleep(1)
-offAllLeds()
+allLeds(OFF)
 
 
 N = 2
@@ -60,7 +62,6 @@ while True:
         led.off()
 
     sleep(0.05)
-
 
 except KeyboardInterrupt:
     offAllLeds()
