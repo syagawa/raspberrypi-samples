3d38500e9efd1f89f87a51b484838fc8ab06f409
syagawa <syagawapp@gmail.com>
2021-02-10 17:20:04 +0900

    Add cache to leds.py

--- modules/leds.py
+++ modules/leds.py
@@ -1,10 +1,20 @@
 from gpiozero import LED, Button, Buzzer, PWMLED
 from time import sleep
+from signal import pause
 
 _leds = [17, 18, 27, 22, 25, 12, 13, 19]
 
 OFF = 0
 ON = 1
+
+DIC = {}
+def cacheLed(num):
+    if num in DIC:
+        return DIC[num]
+    else:
+        L = LED(num)
+        DIC[num] = L
+        return L
 
 def getLed(n):
     _n = n - 1
@@ -14,27 +24,25 @@ def getLed(n):
         print("not exists")
     else:
         num = _leds[_n]
-        led = LED(num)
+        led = cacheLed(num)
         return led
 
 def allFromLeft():
     a = []
     for i in _leds:
-        ld = LED(i)
+        ld = cacheLed(i)
         a.append(ld)
     return a
 
 def allFromRight():
     a = []
     for i in (reversed(_leds)):
-        ld = LED(i)
+        ld = cacheLed(i)
         a.append(ld)
     return a
 
 def all():
     return allFromLeft()
-
-
 
 def setOn(arr):
     for ld in arr:
@@ -48,18 +56,24 @@ def setAllLedsOn():
     arr = all()
     setOn(arr)
 
+def setAllLedsOff():
+    arr = all()
+    setOff(arr)
+
 
 def waveFromLeft():
     arr = allFromLeft()
     for ld in arr:
         ld.on()
         sleep(0.1)
+        ld.off()
 
 def waveFromRight():
     arr = allFromRight()
     for ld in arr:
         ld.on()
         sleep(0.1)
+        ld.off()
 
 def allLeds(mode):
     for i in _leds:
