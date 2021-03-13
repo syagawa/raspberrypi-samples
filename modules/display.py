38438519f313ae6031e2859a1d26c14d755d7459
syagawa <syagawapp@gmail.com>
2021-03-13 11:17:30 +0900

    Fix display.py

--- modules/display.py
+++ modules/display.py
@@ -2,6 +2,7 @@
 # sudo pip3 install Adafruit-Blinka
 # sudo pip3 install adafruit-circuitpython-ssd1306
 # sudo apt install python3-pil
+# for 128 * 64 display
 
 import time
 import subprocess
@@ -17,33 +18,43 @@ display_reset = digitalio.DigitalInOut(board.D4)
 
 w = 128
 h = 64
+rows = 8
+columns = 21
 
 white = 255
 black = 0
+
+pad = -2
+top = pad
+bottom = h - pad
+line_height = 8
+x = 0
+
 
 I2C = board.I2C()
 d = adafruit_ssd1306.SSD1306_I2C(w, h, I2C, addr=0x3C, reset=display_reset)
 
 d.fill(0)
-
 d.show()
-
-
 image = Image.new("1", (d.width, d.height))
 draw = ImageDraw.Draw(image)
+font = ImageFont.load_default()
+
 
 def drawBlackRect():
   draw.rectangle((0, 0, w, h), outline=0, fill=black)
 
+def drawWhiteRect():
+  draw.rectangle((0, 0, w, h), outline=0, fill=white)
+
+def displayMessage(mes, line):
+  l = line - 1
+  draw.text( (x, top + (line_height * l) ), mes, font=font, fill=white)
+
+
+
 drawBlackRect()
 
-pad = -2
-top = pad
-bottom = h - pad
-
-x = 0
-
-font = ImageFont.load_default()
 
 count = 0
 
@@ -56,7 +67,8 @@ while True:
   i = random.randint(0, m_len - 1)
   message = "%s %s" % (messages[i], count)
 
-  draw.text( (x, top), message, font=font, fill=white)
+  l = random.randint(1, 8)
+  displayMessage(message, l)
 
   d.image(image)
   d.show()
